e00f8d52d547cffd319727f1e8e33fe56d6c662b
Paolo-Monti <48802688+Paolo-Monti@users.noreply.github.com>
2020-12-24 11:58:31 +0100

    Update parser.ps1

--- parser.ps1
+++ parser.ps1
@@ -1,2 +1,2 @@
-try { $html = Invoke-WebRequest "https://deviceatlas.com/blog/list-of-user-agent-strings" } catch { Write-Host "Errore"; Exit }
-$data = $html.ParsedHtml.getElementsByTagName('td')
+$html = wget -Uri "http://useragentstring.com/pages/useragentstring.php?typ=Browser"
+@( ($html.Links | where { $_.href.Contains('index.php') }).innerText ) | ogv -Title 'User agents'

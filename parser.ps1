9c3b125a91c05280081b579564c2edb088997be9
Paolo-Monti <48802688+Paolo-Monti@users.noreply.github.com>
2020-12-24 11:59:23 +0100

    Update parser.ps1

--- parser.ps1
+++ parser.ps1
@@ -1,2 +1,1 @@
-$html = wget -Uri "http://useragentstring.com/pages/useragentstring.php?typ=Browser"
-@( ($html.Links | where { $_.href.Contains('index.php') }).innerText ) | ogv -Title 'User agents'
+

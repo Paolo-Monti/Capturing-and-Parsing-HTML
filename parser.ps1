a169d24d8175d6718b79ca587473b95cfd44d32f
Paolo-Monti <48802688+Paolo-Monti@users.noreply.github.com>
2020-12-24 12:00:04 +0100

    Update parser.ps1

--- parser.ps1
+++ parser.ps1
@@ -1,1 +1,4 @@
 
+try { $html = Invoke-WebRequest "https://deviceatlas.com/blog/list-of-user-agent-strings" } catch { Write-Host "Errore"; Exit }
+$data = $html.ParsedHtml.getElementsByTagName('td')
+@( ($data | Where-Object { $_.innerText -notmatch 'bot' }).innerText ) | Out-GridView -Title 'User agents'
